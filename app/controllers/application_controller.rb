3ff2ddbda9c27900d79b45962b00f6a282ca3c36
AsyaGr <agranovs@chicagobooth.edu>
2022-02-08 01:17:00 +0000

    Commit2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
 def blank_square_form
   render({ :template =>"calculation_templates/square_form.html.erb" })
 end
+  def calculate_square
+    # params = {"elephant"=>"42"}
+    @num = params.fetch("elephant").to_i
+    @square_of_num = @num * @num 
+    render({ :template =>"calculation_templates/square_results.html.erb" })
+  end
 end
